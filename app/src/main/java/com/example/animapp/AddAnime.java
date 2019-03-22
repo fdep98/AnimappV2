e5cb6d0cf998662799229bba9155cadbb215d8bc
Jona-659 <jon.affrye@gmail.com>
2019-03-22 17:01:18 +0100

    mise à jour layout + liste d'animé

---
+++ app/src/main/java/com/example/animapp/AddAnime.java
@@ -0,0 +1,13 @@
+package com.example.animapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddAnime extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_anime);
+    }
+}
