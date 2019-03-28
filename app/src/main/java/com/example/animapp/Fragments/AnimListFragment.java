0be0914bab934cb33a22830a8e681a8fac0a400e
Jona-659 <jon.affrye@gmail.com>
2019-03-29 00:17:46 +0100

    toolbar + listView updapte

--- app/src/main/java/com/example/animapp/Fragments/AnimListFragment.java
+++ app/src/main/java/com/example/animapp/Fragments/AnimListFragment.java
@@ -1,20 +1,30 @@
 package com.example.animapp.Fragments;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.button.MaterialButton;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.view.menu.MenuBuilder;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.animapp.AddAnime;
 import com.example.animapp.Model.User;
@@ -43,18 +53,23 @@ public class AnimListFragment extends Fragment {
     private ListView list;
     private FirebaseUser currentUser;
     private DocumentReference userRef;
-    private MaterialButton addAnim;
+    private FloatingActionButton addAnim;
     private FirebaseAuth mAuth;
     ArrayList<User> animListe;
     private FirebaseFirestore firestoreDb; //instance de la BDD firestore
+    private MyListAdapter adapter;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_liste, container,false);
+        View v = inflater.inflate(R.layout.activity_liste, container,false);
+        setHasOptionsMenu(true);
+        return v;
+
 
     }
 
+    @SuppressLint("ResourceType")
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -64,24 +79,59 @@ public class AnimListFragment extends Fragment {
         firestoreDb = FirebaseFirestore.getInstance();
         userRef = firestoreDb.collection("users").document(currentUser.getEmail()); //réference vers le doc du moniteur connecté
 
-        addAnim = getView().findViewById(R.id.ajouter);
         list = view.findViewById(R.id.list);
-        addAnim.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getActivity(), AddAnime.class));
 
-            }
-        });
 
+
+        Toolbar myToolbar = (Toolbar) getView().findViewById(R.id.toolbar);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(myToolbar);
+        //myToolbar.setNavigationIcon(getResources().getDrawable(R.drawable.ic_search_black));
 
         bindAnimeList();
     }
 
     @Override
-    public void onStart() {
-        super.onStart();
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.toolbar_menu, menu);
+
+        MenuItem addAnime = menu.findItem(R.id.ajouter);
+        addAnime.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                startActivity(new Intent(getActivity(), AddAnime.class));
+                return true;
+            }
+        });
+
+        MenuItem updateAbsc = menu.findItem(R.id.updateAbs);
+        updateAbsc.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                //pour chaque item cliqué, mettre à jour le nbr d'abscences
+                return true;
+            }
+        });
+
+
+        MenuItem searchItem = menu.findItem(R.id.search); //reférence vers l'iconne de recherche
+        SearchView searchView = (SearchView) searchItem.getActionView();
+        //searchView.setBackgroundColor(Color.WHITE);
+        searchView.setImeOptions(EditorInfo.IME_ACTION_DONE); //remplace l'action bouton recherche dans le clavier par le v
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                MyListAdapter adapter = new MyListAdapter(getActivity(),R.layout.list_details,animListe);
+                adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
     }
+
 
     public void bindAnimeList(){
         animListe = new ArrayList<>();
@@ -111,7 +161,7 @@ public class AnimListFragment extends Fragment {
                                                     animListe.add(anime);
 
                                                     //Utilisation de notre adaptateur qui se chargera de placer les valeurs de notre liste automatiquement et d'affecter un tag à nos checkbox
-                                                    MyListAdapter adapter = new MyListAdapter(getActivity(),R.layout.list_details,animListe);
+                                                    adapter = new MyListAdapter(getActivity(),R.layout.list_details,animListe);
                                                     list.setAdapter(adapter);
                                                 }
                                             }
--- app/src/main/java/com/example/animapp/MyListAdapter.java
+++ app/src/main/java/com/example/animapp/MyListAdapter.java
@@ -11,6 +11,8 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.CheckBox;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
@@ -18,10 +20,14 @@ import com.example.animapp.Model.User;
 import com.example.animapp.animapp.R;
 
 
-public class MyListAdapter extends ArrayAdapter<User>{
+public class MyListAdapter extends ArrayAdapter<User> implements Filterable {
+
     private LayoutInflater	mInflater;
     private Context mContext;
     private int mResource;
+    private ArrayList<User> userList = null;
+
+    private ArrayList<User> exampleList;
 
     private static class ViewHolder{
         TextView nom;
@@ -33,6 +39,8 @@ public class MyListAdapter extends ArrayAdapter<User>{
         super(context,resource, userList);
         mContext = context;
         mResource = resource;
+        exampleList = new ArrayList<>(userList); //créer une copie de userList
+        this.userList = userList;
     }
 
 
@@ -67,8 +75,45 @@ public class MyListAdapter extends ArrayAdapter<User>{
         holder.pseudo.setText(anime.getPseudo());
         holder.nbrAbsences.setText(anime.getAbsences());
 
-        return convertView;
+        return result;
 
     }
+
+    @Override
+    public Filter getFilter() {
+        return exampleFilter;
+    }
+
+    private Filter exampleFilter = new Filter() {
+
+        //il est exécuté dans un thread en arrière plan
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            ArrayList<User> filteredList = new ArrayList<>();
+
+            //si le filtre est vide, on montre tt les résultats
+            if(constraint == null || constraint.length() == 0){
+                filteredList.addAll(exampleList);
+            }else{
+                String filterPattern = constraint.toString().toLowerCase().trim();
+                for(User user:exampleList){
+                    //check si le mot a filtrer match avec le nom ou le pseudo d'un animé dans la liste
+                    if(user.getNom().toLowerCase().contains(filterPattern) || (user.getPseudo().toLowerCase().contains(filterPattern))){
+                        filteredList.add(user);
+                    }
+                }
+            }
+            FilterResults results = new FilterResults();
+            results.values = filteredList;
+            return results;
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            exampleList.clear();
+            exampleList.addAll((ArrayList) results.values);
+            notifyDataSetChanged();
+        }
+    };
 
 }
